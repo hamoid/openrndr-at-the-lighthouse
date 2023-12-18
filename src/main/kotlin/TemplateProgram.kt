a80fa1918d41b1f5935a878547a51a16c5f7ed7f
Abe Pazos <abe@hamoid.com>
2023-12-18 22:16:22 +0100

    Set window location

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,31 +1,33 @@
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
-import org.openrndr.draw.loadFont
-import org.openrndr.draw.loadImage
-import org.openrndr.draw.tint
+import org.openrndr.math.IntVector2
 import kotlin.math.cos
 import kotlin.math.sin
 
-fun main() = application {
+fun main(args: Array<String>) = application {
     configure {
-        width = 768
-        height = 576
+        width = 1920 / 2
+        height = 1080 / 2
+        if (args.isNotEmpty()) {
+            println("the arg is ${args[0]}")
+            val n = args[0].toInt()
+            if (n in 0..3)
+                position = IntVector2(
+                    (n % 2) * 1920 / 2,
+                    (n / 2) * 1080 / 2 + 1
+                )
+        }
+        hideWindowDecorations = true
     }
 
     program {
-        val image = loadImage("data/images/pm5544.png")
-        val font = loadFont("data/fonts/default.otf", 64.0)
-
         extend {
-            drawer.drawStyle.colorMatrix = tint(ColorRGBa.WHITE.shade(0.2))
-            drawer.image(image)
-
+            val t = System.currentTimeMillis() * 0.001
             drawer.fill = ColorRGBa.PINK
-            drawer.circle(cos(seconds) * width / 2.0 + width / 2.0, sin(0.5 * seconds) * height / 2.0 + height / 2.0, 140.0)
-
-            drawer.fontMap = font
-            drawer.fill = ColorRGBa.WHITE
-            drawer.text("OPENRNDR", width / 2.0, height / 2.0)
+            drawer.circle(
+                cos(t) * width / 2.0 + width / 2.0,
+                sin(0.5 * t) * height / 2.0 + height / 2.0, 140.0
+            )
         }
     }
 }
